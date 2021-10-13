e149f23aab34a5d27c60c5874049cf9fdf7a43a7
MaoLongLong <382084620@qq.com>
2021-10-13 18:14:41 +0800

    feat: add interface define

--- mcts.go
+++ mcts.go
@@ -1,1 +1,16 @@
+// Copyright 2021 go-mcts. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package mcts
+
+type Move interface{}
+
+type State interface {
+	PlayerToMove() int
+	HasMoves() bool
+	GetMoves() []Move
+	DoMove()
+	DoRandomMove()
+	GetResult() float64
+}
